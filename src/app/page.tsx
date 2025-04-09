e55aaed960743e1062dd49579cd30971bc508edb
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-09 15:04:32 +0300

    Add ServicesPage component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Button from '../components/Button';
 import Card from '../components/Card';
 import Layout from '../components/Layout';
 import BookingPage from '../components/BookingPage';
+import ServicesPage from '../components/ServicesPage';
 
 export default function HomePage() {
   return (
@@ -33,6 +34,7 @@ export default function HomePage() {
     <Card />
     <Layout />
     <BookingPage />
+    <ServicesPage />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
