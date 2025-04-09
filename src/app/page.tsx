29ebdbfff1f5352c7f5b2e24e0a903872e9e3fa0
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-09 14:57:55 +0300

    Add Button component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import TestimonialsSection from '../components/TestimonialsSection';
 import TeamSection from '../components/TeamSection';
 import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
+import Button from '../components/Button';
 
 export default function HomePage() {
   return (
@@ -25,6 +26,7 @@ export default function HomePage() {
     <TeamSection />
     <ContactSection />
     <Footer />
+    <Button />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
